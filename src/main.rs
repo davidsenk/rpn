a0d3ad8f018a0691ba35e52262493ebd1dd0cd63
David Senk <david@senk.me>
2023-09-07 13:07:55 -0400

    input loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::io;
+use std::io::Write;
+
 fn main() {
     println!("Hello, world!");
+
+    loop {
+        let mut input = String::new();
+        print!("> ");
+
+        io::stdout()
+            .flush()
+            .expect("flush to stdout should not fail");
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("failed to read line");
+
+        let output = input.trim();
+
+        println!("input received: '{output}'");
+    }
 }
